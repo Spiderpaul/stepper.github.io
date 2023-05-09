1f9a5f2f3681f4c81130c27b5b7f14832c797b33
Paul Lugo <paulugortega@gmail.com>
2023-05-09 14:27:01 -0700

    Cambio en base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/stepper.github.io/'
+  base: 'https://github.com/Spiderpaul/stepper.github.io/'
 })
